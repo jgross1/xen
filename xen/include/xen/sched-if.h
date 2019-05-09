05add663c6adec49acf63f86152c3b0435830014
Juergen Gross <jgross@suse.com>
2019-05-09 09:26:49 +0200

    xen/sched: modify cpupool_domain_cpumask() to be an unit mask
    cpupool_domain_cpumask() is used by scheduling to select cpus or to
    iterate over cpus. In order to support scheduling units spanning
    multiple cpus let cpupool_domain_cpumask() return a cpumask with only
    one bit set per scheduling resource.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/include/xen/sched-if.h
+++ xen/include/xen/sched-if.h
@@ -22,6 +22,8 @@ extern cpumask_t cpupool_free_cpus;
 #define SCHED_DEFAULT_RATELIMIT_US 1000
 extern int sched_ratelimit_us;
 
+/* Scheduling resource mask. */
+extern const cpumask_t *sched_res_mask;
 
 /*
  * In order to allow a scheduler to remap the lock->cpu mapping,
@@ -508,6 +510,7 @@ struct cpupool
 {
     int              cpupool_id;
     cpumask_var_t    cpu_valid;      /* all cpus assigned to pool */
+    cpumask_var_t    res_valid;      /* all scheduling resources of pool */
     struct cpupool   *next;
     unsigned int     n_dom;
     struct scheduler *sched;
@@ -524,7 +527,7 @@ static inline cpumask_t* cpupool_domain_cpumask(const struct domain *d)
      * be interested in calling this for the idle domain.
      */
     ASSERT(d->cpupool != NULL);
-    return d->cpupool->cpu_valid;
+    return d->cpupool->res_valid;
 }
 
 /*
