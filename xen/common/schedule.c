deea35ad6509dc4b0aab5eae9be5defd7a86b198
Juergen Gross <jgross@suse.com>
2019-05-21 17:24:50 +0200

    xen/sched: support multiple cpus per scheduling resource
    Prepare supporting multiple cpus per scheduling resource by allocating
    the cpumask per resource dynamically.
    
    Modify sched_res_mask to have only one bit per scheduling resource set.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/common/schedule.c
+++ xen/common/schedule.c
@@ -59,7 +59,7 @@ integer_param("sched_ratelimit_us", sched_ratelimit_us);
 enum sched_gran opt_sched_granularity = SCHED_GRAN_cpu;
 unsigned int sched_granularity = 1;
 bool sched_disable_smt_switching;
-const cpumask_t *sched_res_mask = &cpumask_all;
+cpumask_var_t sched_res_mask;
 
 /* Common lock for free cpus. */
 static DEFINE_SPINLOCK(sched_free_cpu_lock);
@@ -2287,8 +2287,14 @@ static int cpu_schedule_up(unsigned int cpu)
     sd = xzalloc(struct sched_resource);
     if ( sd == NULL )
         return -ENOMEM;
+    if ( !zalloc_cpumask_var(&sd->cpus) )
+    {
+        xfree(sd);
+        return -ENOMEM;
+    }
+
     sd->processor = cpu;
-    sd->cpus = cpumask_of(cpu);
+    cpumask_copy(sd->cpus, cpumask_of(cpu));
     set_sched_res(cpu, sd);
 
     sd->scheduler = &sched_idle_ops;
@@ -2299,6 +2305,8 @@ static int cpu_schedule_up(unsigned int cpu)
 
     /* We start with cpu granularity. */
     sd->granularity = 1;
+
+    cpumask_set_cpu(cpu, sched_res_mask);
 
     /* Boot CPU is dealt with later in schedule_init(). */
     if ( cpu == 0 )
@@ -2335,6 +2343,7 @@ static void sched_res_free(struct rcu_head *head)
 {
     struct sched_resource *sd = container_of(head, struct sched_resource, rcu);
 
+    free_cpumask_var(sd->cpus);
     xfree(sd);
 }
 
@@ -2482,6 +2491,9 @@ void __init scheduler_init(void)
         BUG_ON(!ops.name);
         printk("Using '%s' (%s)\n", ops.name, ops.opt_name);
     }
+
+    if ( !zalloc_cpumask_var(&sched_res_mask) )
+        BUG();
 
     if ( cpu_schedule_up(0) )
         BUG();
