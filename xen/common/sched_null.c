50d85d5f5c286ba4737cbefb1d946164426368d2
Juergen Gross <jgross@suse.com>
2019-07-19 15:47:16 +0200

    xen/sched: introduce struct sched_resource
    Add a scheduling abstraction layer between physical processors and the
    schedulers by introducing a struct sched_resource. Each scheduler unit
    running is active on such a scheduler resource. For the time being
    there is one struct sched_resource per cpu, but in future there might
    be one for each core or socket only.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/common/sched_null.c
+++ xen/common/sched_null.c
@@ -342,6 +342,7 @@ static void vcpu_assign(struct null_private *prv, struct vcpu *v,
 {
     per_cpu(npc, cpu).vcpu = v;
     v->processor = cpu;
+    v->sched_unit->res = get_sched_res(cpu);
     cpumask_clear_cpu(cpu, &prv->cpus_free);
 
     dprintk(XENLOG_G_INFO, "%d <-- %pv\n", cpu, v);
@@ -420,6 +421,7 @@ static void null_unit_insert(const struct scheduler *ops,
  retry:
 
     cpu = v->processor = pick_cpu(prv, v);
+    unit->res = get_sched_res(cpu);
 
     spin_unlock(lock);
 
@@ -666,6 +668,7 @@ static void null_unit_migrate(const struct scheduler *ops,
      * by this, will be fixed-up during resume.
      */
     v->processor = new_cpu;
+    unit->res = get_sched_res(new_cpu);
 }
 
 #ifndef NDEBUG
