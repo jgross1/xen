2732134ee07116f46b28e7df98f60c5b642ef193
Juergen Gross <jgross@suse.com>
2019-07-19 15:47:19 +0200

    xen/sched: prepare per-cpupool scheduling granularity
    On- and offlining cpus with core scheduling is rather complicated as
    the cpus are taken on- or offline one by one, but scheduling wants them
    rather to be handled per core.
    
    As the future plan is to be able to select scheduling granularity per
    cpupool prepare that by storing the granularity in struct cpupool and
    struct sched_resource (we need it there for free cpus which are not
    associated to any cpupool). Free cpus will always use granularity 1.
    
    Store the selected granularity option (cpu, core or socket) in the
    cpupool as well, as we will need it to select the appropriate cpu mask
    when populating the cpupool with cpus.
    
    This will make on- and offlining of cpus much easier and avoids
    writing code which would needed to be thrown away later.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/common/schedule.c
+++ xen/common/schedule.c
@@ -56,7 +56,8 @@ int sched_ratelimit_us = SCHED_DEFAULT_RATELIMIT_US;
 integer_param("sched_ratelimit_us", sched_ratelimit_us);
 
 /* Number of vcpus per struct sched_unit. */
-static unsigned int sched_granularity = 1;
+enum sched_gran opt_sched_granularity = SCHED_GRAN_cpu;
+unsigned int sched_granularity = 1;
 bool sched_disable_smt_switching;
 const cpumask_t *sched_res_mask = &cpumask_all;
 
@@ -350,10 +351,10 @@ static struct sched_unit *sched_alloc_unit(struct vcpu *v)
 {
     struct sched_unit *unit, **prev_unit;
     struct domain *d = v->domain;
+    unsigned int gran = d->cpupool ? d->cpupool->granularity : 1;
 
     for_each_sched_unit ( d, unit )
-        if ( unit->vcpu->vcpu_id / sched_granularity ==
-             v->vcpu_id / sched_granularity )
+        if ( unit->vcpu->vcpu_id / gran == v->vcpu_id / gran )
             break;
 
     if ( unit )
@@ -1695,11 +1696,11 @@ static void sched_switch_units(struct sched_resource *sd,
         if ( is_idle_unit(prev) )
         {
             prev->runstate_cnt[RUNSTATE_running] = 0;
-            prev->runstate_cnt[RUNSTATE_runnable] = sched_granularity;
+            prev->runstate_cnt[RUNSTATE_runnable] = 1;
         }
         if ( is_idle_unit(next) )
         {
-            next->runstate_cnt[RUNSTATE_running] = sched_granularity;
+            next->runstate_cnt[RUNSTATE_running] = 1;
             next->runstate_cnt[RUNSTATE_runnable] = 0;
         }
     }
@@ -1946,11 +1947,12 @@ static struct sched_unit *sched_wait_rendezvous_in(struct sched_unit *prev,
 {
     struct sched_unit *next;
     struct vcpu *v;
+    unsigned int gran = get_sched_res(cpu)->granularity;
 
     if ( !--prev->rendezvous_in_cnt )
     {
         next = do_schedule(prev, now, cpu);
-        atomic_set(&next->rendezvous_out_cnt, sched_granularity + 1);
+        atomic_set(&next->rendezvous_out_cnt, gran + 1);
         return next;
     }
 
@@ -2054,6 +2056,7 @@ static void schedule(void)
     struct sched_resource *sd;
     spinlock_t           *lock;
     int cpu = smp_processor_id();
+    unsigned int          gran = get_sched_res(cpu)->granularity;
 
     ASSERT_NOT_IN_ATOMIC();
 
@@ -2079,11 +2082,11 @@ static void schedule(void)
 
     stop_timer(&sd->s_timer);
 
-    if ( sched_granularity > 1 )
+    if ( gran > 1 )
     {
         cpumask_t mask;
 
-        prev->rendezvous_in_cnt = sched_granularity;
+        prev->rendezvous_in_cnt = gran;
         cpumask_andnot(&mask, sd->cpus, cpumask_of(cpu));
         cpumask_raise_softirq(&mask, SCHED_SLAVE_SOFTIRQ);
         next = sched_wait_rendezvous_in(prev, lock, cpu, now);
@@ -2149,6 +2152,9 @@ static int cpu_schedule_up(unsigned int cpu)
     sd->schedule_lock = &sd->_lock;
     init_timer(&sd->s_timer, s_timer_fn, NULL, cpu);
     atomic_set(&sd->urgent_count, 0);
+
+    /* We start with cpu granularity. */
+    sd->granularity = 1;
 
     /* Boot CPU is dealt with later in schedule_init(). */
     if ( cpu == 0 )
@@ -2495,6 +2501,7 @@ int schedule_cpu_switch(unsigned int cpu, struct cpupool *c)
     sched_free_pdata(old_ops, ppriv_old, cpu);
 
  out:
+    get_sched_res(cpu)->granularity = c ? c->granularity : 1;
     get_sched_res(cpu)->cpupool = c;
     /* When a cpu is added to a pool, trigger it to go pick up some work */
     if ( c != NULL )
