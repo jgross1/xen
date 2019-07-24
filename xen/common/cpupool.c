575ae229d0a7f0d36cc36f7b1cfdd144760d79a7
Juergen Gross <jgross@suse.com>
2019-07-24 11:03:49 +0200

    xen/sched: prepare per-cpupool scheduling granularity
    On- and offlining cpus with core scheduling is rather complicated as
    the cpus are taken on- or offline one by one, but scheduling wants them
    rather to be handled per core.
    
    As the future plan is to be able to select scheduling granularity per
    cpupool prepare that by storing the granularity in struct cpupool and
    struct sched_resource (we need it there for free cpus which are not
    associated to any cpupool). Free cpus will always use granularity 1.
    
    Store the selected granularity option (cpu, core or socket) in the
    cpupool as well, as we will need it to select the appropriate cpu mask
    when populating the cpupool with cpus.
    
    This will make on- and offlining of cpus much easier and avoids
    writing code which would needed to be thrown away later.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/common/cpupool.c
+++ xen/common/cpupool.c
@@ -175,6 +175,8 @@ static struct cpupool *cpupool_create(
             return NULL;
         }
     }
+    c->granularity = sched_granularity;
+    c->opt_granularity = opt_sched_granularity;
 
     *q = c;
 
