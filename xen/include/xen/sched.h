442872b27ef271c83c1d48699459d91a1b8706f4
Juergen Gross <jgross@suse.com>
2019-07-24 11:03:47 +0200

    xen/sched: use new sched_unit instead of vcpu in scheduler interfaces
    In order to prepare core- and socket-scheduling use a new struct
    sched_unit instead of struct vcpu for interfaces of the different
    schedulers.
    
    Rename the per-scheduler functions insert_vcpu and remove_vcpu to
    insert_unit and remove_unit to reflect the change of the parameter.
    In the schedulers rename local functions switched to sched_unit, too.
    
    For now this new struct will contain a domain, a vcpu pointer and a
    unit_id only and is allocated at vcpu creation time.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Dario Faggioli <dfaggioli@suse.com>

--- xen/include/xen/sched.h
+++ xen/include/xen/sched.h
@@ -140,6 +140,7 @@ void evtchn_destroy(struct domain *d); /* from domain_kill */
 void evtchn_destroy_final(struct domain *d); /* from complete_domain_destroy */
 
 struct waitqueue_vcpu;
+struct sched_unit;
 
 struct vcpu
 {
@@ -160,6 +161,7 @@ struct vcpu
 
     struct timer     poll_timer;    /* timeout for SCHEDOP_poll */
 
+    struct sched_unit *sched_unit;
     void            *sched_priv;    /* scheduler-specific data */
 
     struct vcpu_runstate_info runstate;
@@ -270,6 +272,12 @@ struct vcpu
     struct vpci_vcpu vpci;
 
     struct arch_vcpu arch;
+};
+
+struct sched_unit {
+    struct domain         *domain;
+    struct vcpu           *vcpu_list;
+    int                    unit_id;
 };
 
 /* Per-domain lock can be recursively acquired in fault handlers. */
