7d3a44bece7360a283b25c5c3e86f84b67ad7432
Juergen Gross <jgross@suse.com>
2019-07-24 11:03:48 +0200

    xen/sched: introduce unit_runnable_state()
    Today the vcpu runstate of a new scheduled vcpu is always set to
    "running" even if at that time vcpu_runnable() is already returning
    false due to a race (e.g. with pausing the vcpu).
    
    With core scheduling this can no longer work as not all vcpus of a
    schedule unit have to be "running" when being scheduled. So the vcpu's
    new runstate has to be selected at the same time as the runnability of
    the related schedule unit is probed.
    
    For this purpose introduce a new helper unit_runnable_state() which
    will save the new runstate of all tested vcpus in a new field of the
    vcpu struct.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- xen/include/xen/sched-if.h
+++ xen/include/xen/sched-if.h
@@ -76,6 +76,29 @@ static inline bool unit_runnable(const struct sched_unit *unit)
     return vcpu_runnable(unit->vcpu_list);
 }
 
+static inline bool unit_runnable_state(const struct sched_unit *unit)
+{
+    struct vcpu *v;
+    bool runnable, ret = false;
+
+    if ( is_idle_unit(unit) )
+        return true;
+
+    for_each_sched_unit_vcpu ( unit, v )
+    {
+        runnable = vcpu_runnable(v);
+
+        v->new_state = runnable ? RUNSTATE_running
+                                : (v->pause_flags & VPF_blocked)
+                                  ? RUNSTATE_blocked : RUNSTATE_offline;
+
+        if ( runnable )
+            ret = true;
+    }
+
+    return ret;
+}
+
 static inline void sched_set_res(struct sched_unit *unit,
                                  struct sched_resource *res)
 {
